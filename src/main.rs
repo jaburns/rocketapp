eecc1c99659a0eb42c2ae9e7d08f5cae3255989f
jaburns <j@jaburns.net>
2021-03-23 22:22:51 -0600

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,63 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+use mysql::prelude::Queryable;
+use mysql::Pool;
+use rocket::State;
+
+#[macro_use]
+extern crate rocket;
+
+#[get("/")]
+fn index(dbpool: State<Pool>) -> String {
+    let mut conn = dbpool.get_conn().unwrap();
+
+    let users = conn
+        .query_map(
+            "SELECT id, username, hashedpass FROM users",
+            |(id, username, hashedpass): (u32, String, String)| {
+                format!("{} {} {}", id, username, hashedpass)
+            },
+        )
+        .unwrap();
+
+    users.join("\n")
+}
+
+fn main() {
+    populate_debug_db();
+
+    let url = "mysql://root:iZR3xjHNtAYVLqIU@localhost:3306/rocketapp";
+    let dbpool = Pool::new(url).unwrap();
+
+    rocket::ignite()
+        .manage(dbpool)
+        .mount("/", routes![index])
+        .launch();
+}
+
+fn populate_debug_db() {
+    let url = "mysql://root:iZR3xjHNtAYVLqIU@localhost:3306/information_schema";
+    let dbpool = Pool::new(url).unwrap();
+    let conn = &mut dbpool.get_conn().unwrap();
+
+    let user_tables = conn
+        .query::<u32, _>("SELECT table_id FROM INNODB_TABLES WHERE name = \"rocketapp/users\"")
+        .unwrap()
+        .len();
+
+    if user_tables == 1 {
+        return;
+    }
+
+    conn.query_drop("CREATE DATABASE rocketapp").unwrap();
+
+    let url = "mysql://root:iZR3xjHNtAYVLqIU@localhost:3306/rocketapp";
+    let dbpool = Pool::new(url).unwrap();
+    let conn = &mut dbpool.get_conn().unwrap();
+
+    conn.query_drop("CREATE TABLE users ( id INT NOT NULL, username VARCHAR(255) NOT NULL, passhash VARCHAR(255), PRIMARY KEY (ID) )").unwrap();
+    conn.query_drop("INSERT INTO users (id, username, passhash) VALUES (0, user0, pass0)")
+        .unwrap();
+    conn.query_drop("INSERT INTO users (id, username, passhash) VALUES (1, user1, pass1)")
+        .unwrap();
+}
